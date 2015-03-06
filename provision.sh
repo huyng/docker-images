aad5108a49d6d67443ce39675689211cc1246ec7
huy <huy@huyng.com>
2015-03-06 09:33:40 -0800

    updated provision

---
+++ provision.sh
@@ -0,0 +1,12 @@
+export DEBIAN_FRONTEND=noninteractive 
+
+apt-get -y update
+apt-get install -y git docker.io
+sudo gpasswd -a ${USER} docker
+sudo service docker.io restart
+
+# install cuda
+wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_6.5-14_amd64.deb -O cuda.deb
+dpkg -i cuda.deb && rm cuda.deb
+apt-get update
+apt-get -y install cuda && apt-get clean &&  rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
