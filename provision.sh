5a181ea1c4796f703d8b7e44df13a4e4a1495348
huy <huy@huyng.com>
2015-03-17 00:45:54 -0700

    adde provision

--- provision.sh
+++ provision.sh
@@ -4,7 +4,6 @@ export DEBIAN_FRONTEND=noninteractive
 
 apt-get -y update
 apt-get install -y git docker.io
-sudo gpasswd -a ${USER} docker
 sudo service docker.io restart
 
 # install cuda
@@ -12,3 +11,21 @@ wget http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/c
 dpkg -i cuda.deb && rm cuda.deb
 apt-get update
 apt-get -y install cuda && apt-get clean &&  rm -rf /var/lib/apt/lists/
+
+sudo yum --enablerepo=epel install -y bash-completion
+sudo pip-2.7 install numpy scipy theano ipython
+sudo yum install -y git
+sudo yum install -y python27
+sudo yum install -y python27-devel
+sudo yum install -y python27-pip
+sudo yum install -y atlas
+sudo yum install -y atlas-devel
+sudo yum install -y gcc-gfortran
+sudo yum install -y htop
+sudo yum install -y docker-io
+sudo gpasswd -a ${USER} docker
+sudo service docker start
+
+
+
+sudo PATH=$PATH:/opt/nvidia/cuda/bin THEANO_FLAGS=mode=FAST_RUN,device=gpu0,floatX=float32 python27 gputest.py
